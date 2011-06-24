1f063f46ec96c6c4f8e2ad4689244a2d2f36d317
mengyao <zhaomengyao@gmail.com>
2011-06-24 14:50:13 -0400

    README file is added

--- src/ssw.h
+++ src/ssw.h
@@ -15,26 +15,49 @@
 #include <string.h>
 #include <emmintrin.h>
 
-/* struct of the alignment result */
+/*! @typedef	struct of the alignment results
+ *  @field score	the alignment score
+ *	@field	ref	1-based position in the reference
+ */
 typedef struct {
 	char score;
 	int32_t ref;	/* 1-based position */
 } alignment_end;
 
-/* Generate query profile rearrange query sequence & calculate the weight of match/mismatch. */
+/*! @function	Generate query profile rearrange query sequence & calculate the weight of match/mismatch. 
+ *  @parameter	read	sequence
+ *  @parameter	weight_match	score for a pair of matched reference and read bases
+ *  @parameter	weight_mismatch	score (absolute value) for a pair of mismached reference and read bases
+ *	@parameter	bias	a number used to expend the max capacity of the values in the scoreing matrix; suggest to set to 4
+ *  @return		pointer to the query profile 
+ */
 __m128i* queryProfile_constructor (const char* read,
 								   unsigned char weight_match,    /* will be used as + */
 								   unsigned char weight_mismatch, /* will be used as - */
 								   unsigned char bias);
 
-/* Transform the reference sequence to a number sequence. */
+/*! @function	Transform the reference sequence to a number sequence. 
+ *	@parameter	ref	reference sequence
+ *	@parameter	refLen	reference length
+ *	@return		reference sequence represented by numbers
+ */
 int32_t* ref_nt2num (const char* ref, int32_t refLen);
 
-/* striped Smith-Waterman
-   Record the highest score of each reference position. 
-   Find the ending position of the best alignment. 
-   Beginning of gap and extention of gap are different. 
-   wight_match > 0, all other weights < 0.
+/*! @function	striped Smith-Waterman
+ *  			Record the highest score of each reference position. 
+ *  			Find the ending position of the optimal and sub-optimal alignment.
+ *  @parameter	ref	reference sequence represented by numbers; can be generated using funceion ref_nt2num
+ *  @parameter	refLen	reference length
+ *  @parameter	readLen	read length
+ *  @parameter	weight_insertB	score (absolute value) for opening a insertion 
+ *  @parameter	weight_insertE	score (absolute value) for extending a insertion 
+ *  @parameter	weight_deletB	score (absolute value) for opening a deletion 
+ *  @parameter	weight_deletE	score (absolute value) for extending a deletion 
+ *  @parameter	vProfile	pointer to the query profile
+ *  @parameter	end_seg	a return value of 0-based segment number of alignment ending position in read; suggest to set to 0
+ *	@parameter	bias	a number used to expend the max capacity of the values in the scoreing matrix; suggest to set to 4
+ *  @return		a pointer to the array of structure alignment_end; the optimal (1st member of the array) and 
+ *				sub-optimal (2nd member of the array) alignment score and ending positions
  */
 alignment_end* smith_waterman_sse2 (const int32_t* ref,
 									int32_t refLen,
