b8785d8946e1d6699088afecb8c1a10929e5f24d
mengyao <zhaomengyao@gmail.com>
2011-06-24 12:51:33 -0400

    written with pure c; Heng's fa/fq parser is added

---
+++ main.c
@@ -0,0 +1,59 @@
+/*  main.c
+ *  Created by Mengyao Zhao on 06/23/11.
+ *	Version 0.1.4
+ *	New features: pure c fasta parser, reach Farrar's speed, embeded run time testing function 
+ */
+
+#include <stdlib.h>
+#include <emmintrin.h>
+#include <zlib.h>
+#include <stdio.h>
+#include "ssw.h"
+#include "kseq.h"
+KSEQ_INIT(gzFile, gzread)
+
+int main (int argc, char * const argv[]) {
+	// validate argument count
+	if (argc < 2) {
+		fprintf (stderr, "USAGE: %s <ref.fa/fq> <reads.fa/fq>\n", argv[0]);
+		exit (1);
+	}
+
+	gzFile ref_fp;
+	kseq_t *ref_seq;
+	int l;
+	int m;
+	ref_fp = gzopen(argv[1], "r");
+	ref_seq = kseq_init(ref_fp);
+	while ((l = kseq_read(ref_seq)) >= 0) {
+		printf("ref_name: %s\n", ref_seq->name.s);
+		/* printf("ref_seq: %s\n", ref_seq->seq.s); */
+		gzFile read_fp = gzopen(argv[2], "r");
+		kseq_t*	read_seq = kseq_init(read_fp);
+		int32_t refLen = strlen(ref_seq->seq.s); 
+		int32_t* ref_num = ref_nt2num (ref_seq->seq.s, refLen);
+		while ((m = kseq_read(read_seq)) >= 0) {
+			printf("read_name: %s\n", read_seq->name.s);
+			printf("read_seq: %s\n", read_seq->seq.s); 
+			
+			int32_t end_seg = 0;
+			int32_t readLen = strlen(read_seq->seq.s);
+			__m128i* vProfile = queryProfile_constructor(read_seq->seq.s, 2, 1, 4);
+			alignment_end* bests = smith_waterman_sse2(ref_num, refLen, readLen, 2, 1, 2, 1, vProfile, &end_seg, 4);
+			free(vProfile);
+			
+			if (bests[0].score != 0) {
+				fprintf(stdout, "max score: %d, end_ref: %d\nmax2 score: %d, end_ref: %d\n", 
+						bests[0].score, bests[0].ref, bests[1].score, bests[1].ref);		
+				}else {
+				fprintf(stdout, "No alignment found for this read.\n");
+			}
+		}
+		kseq_destroy(read_seq);
+		gzclose(read_fp);
+	}
+	kseq_destroy(ref_seq);
+	gzclose(ref_fp);
+	return 0;
+
+}
--- ssw.h
+++ ssw.h
@@ -11,42 +11,42 @@
  *
  */
 
-using namespace std;
-#include <vector>
 #include <stdio.h>
 #include <string.h>
 #include <emmintrin.h>
 
-// struct of the alignment result
+/* struct of the alignment result */
 typedef struct {
 	char score;
-	unsigned int ref;	// 1-based position
+	int32_t ref;	/* 1-based position */
 } alignment_end;
 
-// Generate query profile：rearrange query sequence & calculate the weight of match/mismatch.
+/* Generate query profile rearrange query sequence & calculate the weight of match/mismatch. */
 __m128i* queryProfile_constructor (const char* read,
-								   unsigned char weight_match,    // will be used as +
-								   unsigned char weight_mismatch, // will be used as -
+								   unsigned char weight_match,    /* will be used as + */
+								   unsigned char weight_mismatch, /* will be used as - */
 								   unsigned char bias);
 
-//Transform the reference sequence to a number sequence.
-unsigned int* ref_amino2num (const char* ref, unsigned int refLen);
+/* Transform the reference sequence to a number sequence. */
+int32_t* ref_nt2num (const char* ref, int32_t refLen);
 
-// striped Smith-Waterman
-// Record the highest score of each reference position. 
-// Find the ending position of the best alignment. 
-// Beginning of gap and extention of gap are different. 
-// wight_match > 0, all other weights < 0.
-alignment_end* smith_waterman_sse2 (const unsigned int* ref,
-									unsigned int refLen,
-								    unsigned int readLen, 
-								    unsigned char weight_insertB, // will be used as -
-								    unsigned char weight_insertE, // will be used as -
-								    unsigned char weight_deletB,  // will be used as -
-								    unsigned char weight_deletE,  // will be used as -
+/* striped Smith-Waterman
+   Record the highest score of each reference position. 
+   Find the ending position of the best alignment. 
+   Beginning of gap and extention of gap are different. 
+   wight_match > 0, all other weights < 0.
+ */
+alignment_end* smith_waterman_sse2 (const int32_t* ref,
+									int32_t refLen,
+								    int32_t readLen, 
+								    unsigned char weight_insertB, /* will be used as - */
+								    unsigned char weight_insertE, /* will be used as - */
+								    unsigned char weight_deletB,  /* will be used as - */
+								    unsigned char weight_deletE,  /* will be used as - */
 								    __m128i* vProfile,
-								    unsigned int* end_seg,        // 0-based segment number of ending  
-									// alignment; The return value is  
-									// meaningful only when  
-									// the return value != 0.
+								    int32_t* end_seg,        /* 0-based segment number of ending   
+																	 alignment; The return value is  
+																	 meaningful only when  
+																     the return value != 0.
+																   */	
 	 							    unsigned char bias);	
