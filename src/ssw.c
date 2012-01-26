0ad1daf85047c6194c5f61a1292e705f62223c36
Mengyao Zhao <zhaomengyao@gmail.com>
2012-01-26 15:05:48 -0500

    define is used to calculate the max score in a vector

--- src/ssw.c
+++ src/ssw.c
@@ -66,6 +66,215 @@ alignment_end* sw_sse2_16 (const char* ref,
 														   alignment beginning point. If this score 
 														   is set to 0, it will not be used */
 	 							    uint8_t bias) {         /* Shift 0 point to a positive value. */
+
+#define max16(m, vm) (vm) = _mm_max_epu8((vm), _mm_srli_si128((vm), 8)); \
+					  (vm) = _mm_max_epu8((vm), _mm_srli_si128((vm), 4)); \
+					  (vm) = _mm_max_epu8((vm), _mm_srli_si128((vm), 2)); \
+					  (vm) = _mm_max_epu8((vm), _mm_srli_si128((vm), 1)); \
+					  (m) = _mm_extract_epi16((vm), 0)
+	
+	uint8_t max = 0;		                     /* the max alignment score */
+	int32_t end_read = 0;
+	int32_t end_ref = 0; /* 1_based best alignment ending point; Initialized as isn't aligned - 0. */
+	int32_t segLen = (readLen + 15) / 16; /* number of segment */
+	
+	/* array to record the largest score of each reference position */
+	uint8_t* maxColumn = (uint8_t*) calloc(refLen, 1); 
+	
+	/* array to record the alignment read ending position of the largest score of each reference position */
+	int32_t* end_read_column = (int32_t*) calloc(refLen, sizeof(int32_t));
+	
+	/* Define 16 byte 0 vector. */
+	__m128i vZero = _mm_set1_epi32(0);
+
+	__m128i* pvHStore = (__m128i*) calloc(segLen, sizeof(__m128i));
+	__m128i* pvHLoad = (__m128i*) calloc(segLen, sizeof(__m128i));
+	__m128i* pvE = (__m128i*) calloc(segLen, sizeof(__m128i));
+	__m128i* pvHmax = (__m128i*) calloc(segLen, sizeof(__m128i));
+
+	int32_t i;
+	int32_t j;
+	for (i = 0; i < segLen; i ++) {
+		pvHStore[i] = vZero;
+		pvE[i] = vZero;
+	}
+	
+	/* 16 byte insertion begin vector */
+	__m128i vInserB = _mm_set1_epi8(weight_insertB);
+	
+	/* 16 byte insertion extention vector */
+	__m128i vInserE = _mm_set1_epi8(weight_insertE);	
+	
+	/* 16 byte deletion begin vector */
+	__m128i vDeletB = _mm_set1_epi8(weight_deletB);	
+
+	/* 16 byte deletion extention vector */
+	__m128i vDeletE = _mm_set1_epi8(weight_deletE);	
+
+	/* 16 byte bias vector */
+	__m128i vBias = _mm_set1_epi8(bias);	
+
+	__m128i vMaxScore = vZero; /* Trace the highest score of the whole SW matrix. */
+	__m128i vMaxMark = vZero; /* Trace the highest score till the previous column. */	
+	__m128i vTemp;
+	int32_t edge;
+	/* outer loop to process the reference sequence */
+	for (i = 0; i < refLen; i ++) {
+		
+		int32_t cmp;
+		__m128i vF = vZero; /* Initialize F value to 0. 
+							   Any errors to vH values will be corrected in the Lazy_F loop. 
+							 */
+		__m128i vH = pvHStore[segLen - 1];
+		vH = _mm_slli_si128 (vH, 1); /* Shift the 128-bit value in vH left by 1 byte. */
+		
+		/* Swap the 2 H buffers. */
+		__m128i* pv = pvHLoad;
+		
+		__m128i vMaxColumn = vZero; /* vMaxColumn is used to record the max values of column i. */
+		
+		__m128i* vP = vProfile + nt_table[(int)ref[i]] * segLen; /* Right part of the vProfile */
+		pvHLoad = pvHStore;
+		pvHStore = pv;
+		
+		/* inner loop to process the query sequence */
+		for (j = 0; j < segLen; j ++) {
+			vH = _mm_adds_epu8(vH, vP[j]);
+			vH = _mm_subs_epu8(vH, vBias); /* vH will be always > 0 */
+
+			/* Get max from vH, vE and vF. */
+			vH = _mm_max_epu8(vH, pvE[j]);
+			vH = _mm_max_epu8(vH, vF);
+			
+			/* Update highest score encountered this far. */
+			vMaxScore = _mm_max_epu8(vMaxScore, vH);
+			vMaxColumn = _mm_max_epu8(vMaxColumn, vH);
+			
+			/* Save vH values. */
+			pvHStore[j] = vH;
+
+			/* Update vE value. */
+			vH = _mm_subs_epu8(vH, vInserB); /* saturation arithmetic, result >= 0 */
+			pvE[j] = _mm_subs_epu8(pvE[j], vInserE);
+			pvE[j] = _mm_max_epu8(pvE[j], vH);
+			
+			/* Update vF value. */
+			vF = _mm_subs_epu8(vF, vDeletE);
+			vF = _mm_max_epu8(vF, vH);
+			
+			/* Load the next vH. */
+			vH = pvHLoad[j];
+		}
+	
+		/* Lazy_F loop: has been revised to disallow adjecent insertion and then deletion, so don't update E(i, j), learn from SWPS3
+		   The computed vF value is for the given column. 
+	       Since we are at the end, we need to shift the vF value over to the next column.
+		 */
+		vF = _mm_slli_si128 (vF, 1);
+		
+		/* Correct the vH values until there are no element in vF that could influence the vH values. */
+		j = 0;
+		vTemp = _mm_subs_epu8(pvHStore[j], vDeletB);
+		vTemp = _mm_subs_epu8(vF, vTemp);
+		vTemp = _mm_cmpeq_epi8(vTemp, vZero); /* result >= 0 */
+		cmp = _mm_movemask_epi8(vTemp);
+		
+		while (cmp != 0xffff) {
+			pvHStore[j] = _mm_max_epu8(pvHStore[j], vF);
+			
+			/* Update highest score incase the new vH value would change it. (New line I added!) */
+			vMaxScore = _mm_max_epu8(vMaxScore, pvHStore[j]);
+			vMaxColumn = _mm_max_epu8(vMaxColumn, pvHStore[j]);
+			
+			/* Update vF value. */
+			vF = _mm_subs_epu8(vF, vDeletE);
+			
+			j ++;
+			if (j >= segLen) {
+				j = 0;
+				vF = _mm_slli_si128 (vF, 1);
+			}
+			vTemp = _mm_subs_epu8(pvHStore[j], vDeletB);
+			vTemp = _mm_subs_epu8(vF, vTemp);
+			vTemp = _mm_cmpeq_epi8(vTemp, vZero); /* result >= 0 */
+			cmp = _mm_movemask_epi8(vTemp);
+		}		
+		/* end of Lazy-F loop */
+		
+		vTemp = _mm_cmpeq_epi8(vMaxMark, vMaxScore);
+		cmp = _mm_movemask_epi8(vTemp);
+		if (cmp != 0xffff) {
+			uint8_t temp; 
+			vMaxMark = vMaxScore;
+			max16(temp, vMaxScore);
+			vMaxScore = vMaxMark;
+			
+			if (temp > max) {
+				max = temp;
+				end_ref = i;
+			
+				/* Store the column with the highest alignment score in order to trace the alignment ending position on read. */
+				for (j = 0; j < segLen; ++j) // keep the H1 vector
+					pvHmax[j] = pvHStore[j];
+			}
+		}
+		
+		/* Record the max score of current column. */	
+		max16(maxColumn[i], vMaxColumn);
+		if (terminate > 0 && maxColumn[i] == terminate) break;
+	} 	
+
+	/* Trace the alignment ending position on read. */
+	uint8_t *t = (uint8_t*)pvHmax;
+	int32_t column_len = segLen * 16;
+	for (i = 0; i < column_len; ++i, ++t) {
+		if (*t == max) {
+			end_read = i / 16 + i % 16 * segLen;
+			break;
+		}
+	}
+
+	/* Find the most possible 2nd best alignment. */
+	alignment_end* bests = (alignment_end*) calloc(2, sizeof(alignment_end));
+	bests[0].score = max;
+	bests[0].ref = end_ref;
+	bests[0].read = end_read;
+	
+	bests[1].score = 0;
+	bests[1].ref = 0;
+	bests[1].read = 0;
+
+	edge = (end_ref - readLen / 2 - 1) > 0 ? (end_ref - readLen / 2 - 1) : 0;
+	for (i = 0; i < edge; i ++) {
+		if (maxColumn[i] > bests[1].score) 
+			bests[1].score = maxColumn[i];
+	}
+	edge = (end_ref + readLen / 2 + 1) > refLen ? refLen : (end_ref + readLen / 2 + 1);
+	for (i = edge; i < refLen; i ++) {
+		if (maxColumn[i] > bests[1].score) 
+			bests[1].score = maxColumn[i];
+	}
+	
+	free(pvHStore);
+	free(maxColumn);
+	free(end_read_column);
+	return bests;
+}
+
+alignment_end* sw_sse2_8 (const char* ref,
+									int8_t* nt_table,
+									int32_t refLen,
+								    int32_t readLen, 
+								    uint8_t weight_insertB, /* will be used as - */
+								    uint8_t weight_insertE, /* will be used as - */
+								    uint8_t weight_deletB,  /* will be used as - */
+								    uint8_t weight_deletE,  /* will be used as - */
+								    __m128i* vProfile,
+									uint8_t terminate,	/* the best alignment score: used to terminate 
+														   the matrix calculation when locating the 
+														   alignment beginning point. If this score 
+														   is set to 0, it will not be used */
+	 							    uint8_t bias) {         /* Shift 0 point to a positive value. */
 	
 	uint8_t max = 0;		                     /* the max alignment score */
 	int32_t end_read = 0;
@@ -256,17 +465,13 @@ alignment_end* sw_sse2_16 (const char* ref,
 
 	edge = (end_ref - readLen / 2 - 1) > 0 ? (end_ref - readLen / 2 - 1) : 0;
 	for (i = 0; i < edge; i ++) {
-		if (maxColumn[i] > bests[1].score) {
+		if (maxColumn[i] > bests[1].score) 
 			bests[1].score = maxColumn[i];
-			bests[1].ref = i;
-		}
 	}
 	edge = (end_ref + readLen / 2 + 1) > refLen ? refLen : (end_ref + readLen / 2 + 1);
 	for (i = edge; i < refLen; i ++) {
-		if (maxColumn[i] > bests[1].score) {
+		if (maxColumn[i] > bests[1].score) 
 			bests[1].score = maxColumn[i];
-			bests[1].ref = i;
-		}		
 	}
 	
 	free(pvHStore);
