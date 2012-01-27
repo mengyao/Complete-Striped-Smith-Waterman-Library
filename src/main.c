e21c3af0450a43c23f11a8359c448cd82987b7bc
Mengyao Zhao <zhaomengyao@gmail.com>
2012-01-27 13:26:45 -0500

    weight options correct, no comments

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 /*  main.c
  *  Created by Mengyao Zhao on 06/23/11.
  *	Version 0.1.4
- *  Last revision by Mengyao Zhao on 01/24/12.
- *	New features: weight matrix is extracted 
+ *  Last revision by Mengyao Zhao on 01/27/12.
+ *	New features: make weight as options 
  */
 
 #include <stdlib.h>
@@ -39,13 +39,10 @@ int main (int argc, char * const argv[]) {
 	kseq_t *ref_seq;
 	int32_t l, m, k, match, mismatch, insert_open, insert_extention, delet_open, delet_extention;
 	int8_t mat[25];
-	//char ref[50], read[50];
 
 	// Parse command line.
 	while ((l = getopt(argc, argv, "m:x:i:e:d:f:")) >= 0) {
 		switch (l) {
-		//	case 'd': ref = optarg; break;
-		//	case 'q': read = optarg; break;
 			case 'm': match = atoi(optarg); break;
 			case 'x': mismatch = atoi(optarg); break;
 			case 'i': insert_open = atoi(optarg); break;
@@ -59,12 +56,6 @@ int main (int argc, char * const argv[]) {
 		fprintf(stderr, "Usage: ssw_test [-m weight match] [-x abs(weight mismatch)] [-i abs(weight insert_open)] [-e abs(weight insert_extention)] [-d abs(weight delet_open)] [-f abs(weight delet_extention)] <target.fa> <query.fa>\n");
 		return 1;
 	}
-	// validate argument count
-/*	if (argc < 2) {
-		fprintf (stderr, "USAGE: %s <ref.fa/fq> <reads.fa/fq>\n", argv[0]);
-		exit (1);
-	}*/
-
 
 	/* This table is used to transform nucleotide letters into numbers. */
 	int8_t nt_table[128] = {
@@ -82,14 +73,11 @@ int main (int argc, char * const argv[]) {
 	for (l = k = 0; l < 4; ++l) {
 		for (m = 0; m < 4; ++m)
 			mat[k++] = l == m ? match : -mismatch;	/* weight_match : -weight_mismatch */
-	//		mat[k++] = l == m ? 2 : -2;	/* weight_match : -weight_mismatch */
 		mat[k++] = 0; // ambiguous base
 	}
 	for (m = 0; m < 5; ++m) mat[k++] = 0;
 
-	//fprintf(stderr, "argv[1]: %s\n", argv[1]);
 	ref_fp = gzopen(argv[optind], "r");
-	//ref_fp = gzopen(ref, "r");
 	ref_seq = kseq_init(ref_fp);
 
 	start = clock();
@@ -98,7 +86,6 @@ int main (int argc, char * const argv[]) {
 		kseq_t *read_seq;
 		printf("ref_name: %s\n", ref_seq->name.s);
 		read_fp = gzopen(argv[optind + 1], "r");
-	//	read_fp = gzopen(read, "r");
 		read_seq = kseq_init(read_fp);
 		int32_t refLen = strlen(ref_seq->seq.s);
 		char* ref_reverse = seq_reverse(ref_seq->seq.s, refLen - 1); 
@@ -112,7 +99,6 @@ int main (int argc, char * const argv[]) {
 			int32_t readLen = strlen(read_seq->seq.s);
 			__m128i* vProfile = queryProfile_constructor(read_seq->seq.s, nt_table, mat, 5, 4);
 			bests = sw_sse2_byte(ref_seq->seq.s, nt_table, refLen, readLen, insert_open, insert_extention, delet_open, delet_extention, vProfile, 0, 4);
-		//	bests = sw_sse2_byte(ref_seq->seq.s, nt_table, refLen, readLen, 3, 1, 3, 1, vProfile, 0, 4);
 			free(vProfile);
 			
 			if (bests[0].score != 0) {
