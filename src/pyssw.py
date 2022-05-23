714460dfad01381d0a576fbb865219dedfc51ce3
Mengyao Zhao <zhaom@pillarbiosci.com>
2022-05-23 10:00:57 -0400

    fixed conflict in pyssw.py

--- src/pyssw.py
+++ src/pyssw.py
@@ -3,7 +3,7 @@
 Simple python wrapper for SSW library
 Please put the path of libssw.so into LD_LIBRARY_PATH or pass it explicitly as a parameter
 By Yongan Zhao (March 2016)
-Revised by Mengyao Zhao on 2022-May-20
+Revised by Mengyao Zhao on 2022-May-23
 """
 
 import sys
@@ -224,13 +224,8 @@ def main(args):
             # assume the format of the input score matrix is the same as that of http://www.ncbi.nlm.nih.gov/Class/FieldGuide/BLOSUM62.txt
             lEle, dEle2Int, dInt2Ele, lScore = ssw_lib.read_matrix(args.sMatrix)
 
-<<<<<<< pyssw_fixes
-    if args.bBest and args.bProtein:
-        print >> sys.stderr, 'Reverse complement alignment is not available for protein sequences.'
-=======
     if args.bBest and args.bProtien:
         sys.stderr.write('Reverse complement alignment is not available for protein sequences.\n')
->>>>>>> master
 
 # translate score matrix to ctypes
     mat = (len(lScore) * ct.c_int8) ()
